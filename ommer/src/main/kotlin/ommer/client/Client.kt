e844e73115f5e4fe2902a0d8f7a2ba5bc9ba3dcc
Henrik Bierbum Bacher <Bacher@gmail.com>
2024-06-25 22:36:30 +0200

    Fix typo

--- ommer/src/main/kotlin/ommer/client/Client.kt
+++ ommer/src/main/kotlin/ommer/client/Client.kt
@@ -55,11 +55,11 @@ fun Duration.formatHMS(): String =
 data class Podcast(val urn: String, val slug: String, val titleSuffix: String?, val descriptionSuffix: String?, val feedUrl: String?, val imageUrl: String?)
 
 fun main(args: Array<String>) {     
-    val slug = args[0] ?! System.getenv("slug")
-    val urn = args[1] ?! System.getenv("urn")
-    val imageUrl = args[2] ?! System.getenv("imageUrl")
-    val apiKey = args[3] ?! System.getenv("apiKey")
-    val baseUrl = args[4] ?! System.getenv("hostUrl")
+    val slug = args[0] ?: System.getenv("slug")
+    val urn = args[1] ?: System.getenv("urn")
+    val imageUrl = args[2] ?: System.getenv("imageUrl")
+    val apiKey = args[3] ?: System.getenv("apiKey")
+    val baseUrl = args[4] ?: System.getenv("hostUrl")
 
     val feedUrl = "https://${baseUrl}/feeds/${slug}.xml"
 
