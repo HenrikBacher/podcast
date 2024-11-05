74db1a7f1e2630436854f9de09cd26124f39453c
Henrik Bierbum Bacher <bacher@gmail.com>
2024-11-05 22:22:37 +0100

    Update dependencies

--- ommer/build.gradle.kts
+++ ommer/build.gradle.kts
@@ -3,9 +3,9 @@ plugins { id("com.github.johnrengelman.shadow") version "8.1.1" }
 dependencies {
     implementation("org.slf4j:slf4j-api:2.0.16")
     implementation("ch.qos.logback:logback-classic:1.5.12")
-    implementation("org.http4k:http4k-core:5.33.1.0")
-    implementation("org.http4k:http4k-client-jetty:5.33.1.0")
-    implementation("org.http4k:http4k-format-gson:5.33.1.0")
+    implementation("io.ktor:ktor-client-core:2.3.1")
+    implementation("io.ktor:ktor-client-cio:2.3.1")
+    implementation("io.ktor:ktor-client-gson:2.3.1")
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
 }
 
