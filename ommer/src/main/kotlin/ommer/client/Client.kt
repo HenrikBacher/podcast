972643901259bf16eb895d2fcfe80c33937825e4
Henrik Bierbum Bacher <bacher@gmail.com>
2024-11-06 21:02:08 +0100

    Fix logging

--- ommer/src/main/kotlin/ommer/client/Client.kt
+++ ommer/src/main/kotlin/ommer/client/Client.kt
@@ -4,7 +4,6 @@ import com.google.gson.Gson
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
@@ -34,9 +33,6 @@ private val client = HttpClient(CIO) {
         gson {
             // Optional: Configure Gson here if needed
         }
-    }
-    install(Logging) {
-        level = LogLevel.ERROR
     }
 }
 
