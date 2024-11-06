a64f315a6c5bf1bc6b3a5613818b1c12279f3260
Henrik Bierbum Bacher <Bacher@gmail.com>
2024-11-06 18:31:40 +0100

    Refactor usage of http4k to okhttp (#66)
    * Use okhttp in place of http4k

--- ommer/src/main/kotlin/ommer/drapi/Data.kt
+++ ommer/src/main/kotlin/ommer/drapi/Data.kt
@@ -1,12 +1,10 @@
 package ommer.drapi
 
-import org.http4k.core.Uri
-
 data class Group(
         val limit: Int,
         val offset: Int,
         val totalSize: Int,
-        val next: Uri?,
+        val next: String?,
         val title: String,
         val groupId: String,
         val items: List<Item>,
@@ -58,9 +56,9 @@ data class Episodes(
         val items: List<Item>,
         val limit: Int,
         val offset: Int,
-        val previous: Uri?,
-        val next: Uri?,
-        val self: Uri?,
+        val previous: String?,
+        val next: String?,
+        val self: String?,
         val totalSiz: Int,
 )
 
@@ -71,7 +69,7 @@ data class Item(
         val audioAssets: List<AudioAsset>,
         val title: String,
         val description: String,
-        val presentationUrl: Uri,
+        val presentationUrl: String,
         val publishTime: String,
 )
 
@@ -81,5 +79,5 @@ data class AudioAsset(
         val format: String,
         val bitrate: Int,
         val fileSize: Long,
-        val url: Uri,
+        val url: String,
 )
