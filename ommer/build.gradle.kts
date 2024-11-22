12e00a2e3764f7c4eb5d61814856cd2785e252ae
Henrik Bierbum Bacher <bacher@gmail.com>
2024-11-22 07:06:48 +0100

    Use springboot

--- ommer/build.gradle.kts
+++ ommer/build.gradle.kts
@@ -1,21 +1,35 @@
-plugins { id("com.github.johnrengelman.shadow") version "8.1.1" }
+plugins {
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+}
 
 val mainClass = "ommer.client.ClientKt"
 
-tasks.jar {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest { attributes["Main-Class"] = mainClass }
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+tasks.bootJar {
+    mainClass.set(mainClass)
+    layered {
+        enabled = true
+        includeLayerTools = true
+    }
 }
 
-tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-    archiveClassifier.set("")
-    manifest { attributes["Main-Class"] = mainClass }
-    minimize()
+configurations.all {
+    exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(module = "spring-boot-starter-json")
+        exclude(module = "jackson-databind")
+    }
+    // Use smaller logging framework
+    implementation("org.slf4j:slf4j-simple")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         incremental = true
+        jvmTarget = "21"
     }
 }
