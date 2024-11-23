8b6ee9e520a57048f9a9493277bfd4b544d89016
Henrik Bierbum Bacher <bacher@gmail.com>
2024-11-23 18:09:26 +0100

    Revert

--- ommer/build.gradle.kts
+++ ommer/build.gradle.kts
@@ -1,11 +1,6 @@
-plugins { 
-    id("com.gradleup.shadow") version "8.3.5" 
-}
+plugins { id("com.gradleup.shadow") version "8.3.5" }
 
 val mainClass = "ommer.client.ClientKt"
-
-dependencies {
-}
 
 tasks.jar {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
@@ -18,26 +13,8 @@ tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest { attributes["Main-Class"] = mainClass }
     mergeServiceFiles()
-    minimize {
-        exclude(dependency("org.jetbrains.kotlin:.*"))
-        exclude(dependency("org.jetbrains.kotlinx:.*"))
-    }
-    mergeServiceFiles {
-        include("META-INF/services/*")
-    }
+    minimize()
     exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
-    exclude("**/*.kotlin_metadata")
-    exclude("**/*.kotlin_module")
-    exclude("**/*.kotlin_builtins")
-    exclude("META-INF/maven/**")
-    exclude("META-INF/versions/**")
-    exclude("**/*.html")
-    exclude("**/*.txt")
-    exclude("**/*.properties")
-    exclude("**/*.xml")
-    exclude("META-INF/DEPENDENCIES")
-    exclude("META-INF/LICENSE")
-    exclude("META-INF/NOTICE")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
