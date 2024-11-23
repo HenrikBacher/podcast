2c9d0768675320bb9180543864c0f4728a35401e
Henrik Bierbum Bacher <bacher@gmail.com>
2024-11-23 18:03:14 +0100

    Revert

--- ommer/build.gradle.kts
+++ ommer/build.gradle.kts
@@ -2,6 +2,12 @@ plugins { id("com.gradleup.shadow") version "8.3.5" }
 
 val mainClass = "ommer.client.ClientKt"
 
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes["Main-Class"] = mainClass }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
+
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     archiveClassifier.set("")
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
