119320491c60e59abb64ffbe289525478c98d49d
Henrik Bierbum Bacher <Bacher@gmail.com>
2025-06-01 06:33:19 +0200

    Add Scriban template for RSS feed generation and update PodcastFeedGenerator

--- src/PodcastFeedGenerator.cs
+++ src/PodcastFeedGenerator.cs
@@ -1,7 +1,8 @@
 using System.Globalization;
 using System.Text.Json;
-using System.Xml.Linq;
 using Microsoft.Extensions.DependencyInjection;
+using Scriban;
+using System.Xml.Linq;
 using Microsoft.Extensions.Http;
 using Polly;
 using Polly.Extensions.Http;
@@ -68,51 +69,8 @@ var tasks = podcastList?.Podcasts.Select(async podcast =>
             Image = GetImageUrlFromAssets(series?.ImageAssets) ?? GetImageUrlFromAssets(podcast.ImageAssets)
         };
 
-        // XML namespaces for RSS/iTunes
-        XNamespace atom = "http://www.w3.org/2005/Atom";
-        XNamespace itunes = "http://www.itunes.com/dtds/podcast-1.0.dtd";
-        XNamespace media = "http://search.yahoo.com/mrss/";
-
-        var channel = new XElement("channel",
-            new XElement(atom + "link",
-                new XAttribute("href", $"{baseUrl}/feeds/{slug}.xml"),
-                new XAttribute("rel", "self"),
-                new XAttribute("type", "application/rss+xml")
-            ),
-            new XElement("title", channelModel.Title),
-            new XElement("link", channelModel.Link),
-            new XElement("description", channelModel.Description),
-            new XElement("language", channelModel.Language),
-            new XElement("copyright", channelModel.Copyright),
-            new XElement("lastBuildDate", channelModel.LastBuildDate),
-            new XElement(itunes + "explicit", channelModel.Explicit),
-            new XElement(itunes + "author", channelModel.Author),
-            new XElement(itunes + "block", channelModel.Block),
-            new XElement(itunes + "owner",
-                new XElement(itunes + "email", channelModel.Owner?.Email),
-                new XElement(itunes + "name", channelModel.Owner?.Name)
-            ),
-            new XElement(itunes + "new-feed-url", channelModel.NewFeedUrl),
-            new XElement(itunes + "image", new XAttribute("href", channelModel.Image ?? ""))
-        );
-
-        // Add iTunes categories from series data using modern collection expressions
-        if (series?.Categories is { Count: > 0 } categories)
-        {
-            foreach (var category in categories.Where(c => !string.IsNullOrEmpty(c)))
-            {
-                channel.Add(new XElement(itunes + "category", new XAttribute("text", category)));
-            }
-        }
-
-        // Add series summary/subtitle if available
-        if (!string.IsNullOrEmpty(series?.Punchline))
-            channel.Add(new XElement(itunes + "subtitle", series.Punchline));
-
-        if (!string.IsNullOrEmpty(series?.Description))
-            channel.Add(new XElement(itunes + "summary", series.Description));
-
-        // Determine iTunes type using pattern matching
+        // Prepare Scriban context
+        var categories = (series?.Categories ?? new List<string>()).Where(c => !string.IsNullOrEmpty(c)).ToList();
         string itunesType = series?.DefaultOrder?.ToLowerInvariant() switch
         {
             "desc" => "episodic",
@@ -130,111 +88,82 @@ var tasks = podcastList?.Podcasts.Select(async podcast =>
             }
         };
 
-        channel.Add(new XElement(itunes + "type", itunesType));
-
-        // Add season information for seasonal content
+        int? seasonCount = null;
         if (series?.GroupingType?.Contains("Seasons", StringComparison.OrdinalIgnoreCase) == true)
         {
-            int seasonCount = series.NumberOfSeasons > 0 ? series.NumberOfSeasons : series.NumberOfSeries;
-            if (seasonCount > 0)
-                channel.Add(new XElement(itunes + "season", seasonCount));
+            seasonCount = series.NumberOfSeasons > 0 ? series.NumberOfSeasons : series.NumberOfSeries;
+            if (seasonCount <= 0) seasonCount = null;
         }
 
-        if (episodes is { Count: > 0 })
+        var channelContext = new
         {
-            // Sort episodes based on the determined podcast type using modern LINQ
-            var sortedEpisodes = itunesType == "serial"
-                ? episodes.OrderBy(ep => DateTime.TryParse(ep.PublishTime, out var dt) ? dt : DateTime.MinValue)
-                : episodes.OrderByDescending(ep => DateTime.TryParse(ep.PublishTime, out var dt) ? dt : DateTime.MinValue);
+            Title = channelModel.Title,
+            Link = channelModel.Link,
+            Description = channelModel.Description,
+            Language = channelModel.Language,
+            Copyright = channelModel.Copyright,
+            LastBuildDate = channelModel.LastBuildDate,
+            Explicit = channelModel.Explicit,
+            Author = channelModel.Author,
+            Block = channelModel.Block,
+            Owner = channelModel.Owner,
+            NewFeedUrl = channelModel.NewFeedUrl,
+            Image = channelModel.Image,
+            Categories = categories,
+            Subtitle = series?.Punchline,
+            Summary = series?.Description,
+            Type = itunesType,
+            Season = seasonCount
+        };
 
-            foreach (var episode in sortedEpisodes)
+        var sortedEpisodes = (episodes ?? new List<Episode>());
+        if (itunesType == "serial")
+            sortedEpisodes = sortedEpisodes.OrderBy(ep => DateTime.TryParse(ep.PublishTime, out var dt) ? dt : DateTime.MinValue).ToList();
+        else
+            sortedEpisodes = sortedEpisodes.OrderByDescending(ep => DateTime.TryParse(ep.PublishTime, out var dt) ? dt : DateTime.MinValue).ToList();
+
+        var episodeContexts = sortedEpisodes.Select(episode =>
+        {
+            string epImage = GetImageUrlFromAssets(episode.ImageAssets) ?? channelModel.Image ?? "";
+            var (epAudio, epAudioLength) = episode.AudioAssets?
+                .Where(a => a?.Format == "mp3")
+                .OrderByDescending(a => a?.Bitrate ?? 0)
+                .FirstOrDefault() is { } best
+                ? (best.Url ?? "", best.FileSize ?? 0)
+                : ("", 0);
+
+            string itunesDuration = episode.DurationMilliseconds > 0
+                ? TimeSpan.FromMilliseconds(episode.DurationMilliseconds.Value).ToString(@"hh\:mm\:ss")
+                : "";
+
+            return new
             {
-                string epTitle = episode.Title ?? "";
-                string epDesc = episode.Description ?? "";
-                string epPubDate = episode.PublishTime ?? "";
-                string epGuid = episode.Id ?? Guid.NewGuid().ToString();
-                string epLink = episode.PresentationUrl ?? "";
-                string? epImage = GetImageUrlFromAssets(episode.ImageAssets) ?? channelModel.Image;
+                Title = episode.Title ?? "",
+                Description = episode.Description ?? "",
+                PublishTime = DateTime.TryParse(episode.PublishTime, out var dt)
+                    ? dt.ToString("ddd, dd MMM yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture)
+                    : episode.PublishTime ?? "",
+                ExplicitContent = episode.ExplicitContent,
+                Image = epImage,
+                Duration = itunesDuration,
+                EpisodeNumber = episode.EpisodeNumber,
+                SeasonNumber = episode.SeasonNumber,
+                PresentationUrl = episode.PresentationUrl,
+                Id = episode.Id ?? Guid.NewGuid().ToString(),
+                AudioUrl = epAudio,
+                AudioLength = epAudioLength,
+                Categories = episode.Categories ?? new List<string>()
+            };
+        }).ToList();
 
-                // Select the highest quality mp3 using LINQ and pattern matching
-                var (epAudio, epAudioLength) = episode.AudioAssets?
-                    .Where(a => a?.Format == "mp3")
-                    .OrderByDescending(a => a?.Bitrate ?? 0)
-                    .FirstOrDefault() is { } best
-                    ? (best.Url ?? "", best.FileSize ?? 0)
-                    : ("", 0);
-
-                // Format iTunes duration using modern TimeSpan formatting
-                string itunesDuration = episode.DurationMilliseconds > 0
-                    ? TimeSpan.FromMilliseconds(episode.DurationMilliseconds.Value).ToString(@"hh\:mm\:ss")
-                    : "";
-
-                var item = new XElement("item",
-                    new XElement("title", epTitle),
-                    new XElement("description", epDesc),
-                    new XElement("pubDate", DateTime.TryParse(epPubDate, out var dt)
-                        ? dt.ToString("ddd, dd MMM yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture)
-                        : epPubDate),
-                    new XElement("explicit", episode.ExplicitContent ? "yes" : "no"),
-                    new XElement(itunes + "author", "DR"),
-                    new XElement(itunes + "image", new XAttribute("href", epImage ?? "")),
-                    new XElement(itunes + "duration", itunesDuration),
-                    new XElement(media + "restriction",
-                        new XAttribute("relationship", "allow"),
-                        new XAttribute("type", "country"),
-                        "dk"
-                    )
-                );
-
-                // Add iTunes episode metadata
-                item.Add(new XElement(itunes + "episodeType", "full"));
-
-                if (episode.EpisodeNumber is > 0)
-                    item.Add(new XElement(itunes + "episode", episode.EpisodeNumber.Value));
-
-                if (episode.SeasonNumber is > 0)
-                    item.Add(new XElement(itunes + "season", episode.SeasonNumber.Value));
-
-                if (!string.IsNullOrEmpty(epLink))
-                    item.Add(new XElement("link", epLink));
-
-                if (!string.IsNullOrEmpty(epGuid))
-                    item.Add(new XElement("guid", new XAttribute("isPermalink", "false"), epGuid));
-
-                if (!string.IsNullOrEmpty(epAudio))
-                {
-                    var enclosure = new XElement("enclosure",
-                        new XAttribute("url", epAudio),
-                        new XAttribute("type", "audio/mpeg"));
-
-                    if (epAudioLength > 0)
-                        enclosure.Add(new XAttribute("length", epAudioLength));
-
-                    item.Add(enclosure);
-                }
-
-                // Add episode categories
-                if (episode.Categories is { Count: > 0 } episodeCategories)
-                {
-                    foreach (var cat in episodeCategories)
-                        item.Add(new XElement(itunes + "category", new XAttribute("text", cat)));
-                }
-
-                channel.Add(item);
-            }
-        }
-
-        var rss = new XElement("rss",
-            new XAttribute("version", "2.0"),
-            new XAttribute(XNamespace.Xmlns + "atom", atom),
-            new XAttribute(XNamespace.Xmlns + "itunes", itunes),
-            new XAttribute(XNamespace.Xmlns + "media", media),
-            channel
-        );
+        // Load and render Scriban template
+        var templateText = await File.ReadAllTextAsync("src/PodcastFeedTemplate.sbn");
+        var template = Template.Parse(templateText);
+        var scribanContext = new { channel = channelContext, episodes = episodeContexts };
+        var result = template.Render(scribanContext, member => member.Name);
 
         string outputPath = Path.Combine("output", $"{slug}.xml");
-        var doc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), rss);
-        doc.Save(outputPath);
+        await File.WriteAllTextAsync(outputPath, result);
         Console.WriteLine($"Saved RSS feed: {outputPath}");
     }
     catch (Exception ex)
